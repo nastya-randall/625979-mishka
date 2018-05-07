49957cedd96e00937921a998ec4fc5414519f9a2
Unknown <anastassiya.sviridova@gmail.com>
2018-05-07 23:17:53 +0200

    Исправила js и ПП

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,6 @@ var navToggle = document.querySelector('.main-nav__toggle');
 var link = document.querySelector(".add-to-cart");
 var popup = document.querySelector(".modal");
 var overlay = document.querySelector(".modal__overlay");
-//var close = document.querySelector(".modal-close");
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -18,20 +17,23 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-link.addEventListener("click", function (evt) {
+if (link) {
+  link.addEventListener("click", function (evt) {
     evt.preventDefault();
-//    overlay.classList.add("modal-show-overlay");
     popup.classList.add("modal--show");
     overlay.classList.add("modal__overlay--show");
-});
+  });
+};
 
-overlay.addEventListener("click", function (evt) {
-  evt.preventDefault ();
-  if (popup.classList.contains("modal--show")) {
-            popup.classList.remove("modal--show");
-            overlay.classList.remove("modal__overlay--show");
-  }
-});
+if (overlay) {
+  overlay.addEventListener("click", function (evt) {
+    evt.preventDefault ();
+    if (popup.classList.contains("modal--show")) {
+      popup.classList.remove("modal--show");
+      overlay.classList.remove("modal__overlay--show");
+    }
+  });
+};
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
