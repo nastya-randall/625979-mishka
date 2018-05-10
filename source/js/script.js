fe8fec4ca650f548353bd42950de8ddd96432d96
Unknown <anastassiya.sviridova@gmail.com>
2018-05-10 17:35:59 +0200

    Поправки ПП

--- source/js/script.js
+++ source/js/script.js
@@ -7,7 +7,7 @@ var overlay = document.querySelector(".modal__overlay");
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
